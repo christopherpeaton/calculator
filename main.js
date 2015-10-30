5035f683a7d0ee7e93a44f5de3756b407ef41fc2
Christopher Eaton <christopherpeaton@gmail.com>
2015-10-29 17:53:44 -0700

    full function w erics code in, my code in js commented out, design needs to be done

--- main.js
+++ main.js
@@ -4,29 +4,28 @@
 
 
 
-var my_calculator
-() = new_calculator(parameters);
-$(document).ready(function () {
-    $("button").on("click", function () {
-        var button_pressed = $(this).text()
-        if (val = clear_entry) {
-            console.log("clear entry")
-            my_calculator.allClear();
-        } else {
-            my_calculator.addItem($(this).text());
-        };
-    });
-});
-
-function parameters(type, value, item) {
-    if (type == value) {
-        if (value == undefined) {
-            console.log("Undefined");
-            $(".display").text("Undefined");
-        } else {
-            console.log("defined");
-            $(".display").text(value);
-        };
-    };
-};
+//var my_calculator() = new_calculator(parameters);
+//$(document).ready(function () {
+//    $("button").on("click", function () {
+//        var button_pressed = $(this).text()
+//        if (val = clear_entry) {
+//            console.log("clear entry")
+//            my_calculator.allClear();
+//        } else {
+//            my_calculator.addItem($(this).text());
+//        };
+//    });
+//});
+//
+//function parameters(type, value, item) {
+//    if (type == value) {
+//        if (value == undefined) {
+//            console.log("Undefined");
+//            $(".display").text("Undefined");
+//        } else {
+//            console.log("defined");
+//            $(".display").text(value);
+//        };
+//    };
+//};
 

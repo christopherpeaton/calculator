b472f37bc82d837508a41b5d14265c10c1ca1007
Christopher Eaton <christopherpeaton@gmail.com>
2015-11-03 10:17:25 -0800

    working on equals sign, doing math

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var i = 0;
 var num_array = [''];
 
 $(document).ready(function () {
-    $(".number_pad").on('click', 'button', function () {
+    $(".number_pad").on('click', 'button:not(#equals)', function () {
         console.log('number clicked');
         //console.log($(this).text());
         var button = $(this).text();
@@ -21,7 +21,17 @@ $(document).ready(function () {
         var button = $(this).text();
         input_operator(button);
     });
+    $("#equals").on("click", function () {
+        opSwitch();
+        math(num1, op, num2);
+        console.log("total");
+    });
 });
+num1 = null;
+num2 = null;
+op = null;
+var i = 0;
+var num_array = [''];
 
 function input_digit(numString) {
     num_array[i] += numString;
@@ -30,62 +40,60 @@ function input_digit(numString) {
 }
 
 function input_operator(op) {
-    i++
+    i++;
     num_array[i] = op;
     i++;
     num_array[i] = '';
-    //do operator stuff here
-    //function operatorSwitch(num1, op, num2) {
-    //    switch (op) {
-    //        case '+':
-    //            total = Number(num1) + Number(num2);
-    //            break;
-    //        case '-':
-    //            total = Number(num1) - Number(num2);
-    //            break;
-    //        case '*':
-    //            total = Number(num1) * Number(num2);
-    //            break;
-    //        case '/':
-    //            if (num2 === 0) {
-    //                alert("need more input");
-    //                return;
-    //            } else {
-    //                total = Number(num1) / Number(num2);
-    //            }
-    //            break;
-    //    }
-    //}
-
-    $(".equals").on("click", 'button', function () {
-        //operatorSwitch();
-        calculate(num1, op, num2);
-        console.log("total")
-    });
-    function calculate(num1, op, num2) {
-        num1 = null;
-        num2 = null;
-        op = null;
-        var i = 0;
-        var num_array = [''];
-        for (i = 0; i < num_array.length; i++) {
-            if (!isNaN(num_array[i])) {
-                //is a number
-                if (num1 === null) {
-                    num1 = num_array[i];
-                    console.log("num1 = i")
-                } else {
-                    num2 = num_array[i];
-                }
+}
+//do operator stuff here
+function opSwitch(num1, op, num2) {
+    switch (op) {
+        case '+':
+            total = Number(num1) + Number(num2);
+            break;
+        case '-':
+            total = Number(num1) - Number(num2);
+            break;
+        case '*':
+            total = Number(num1) * Number(num2);
+            break;
+        case '/':
+            if (num2 === 0) {
+                alert("need more input");
+                return;
             } else {
-                // is an op
-                op = num_array[i];
+                total = Number(num1) / Number(num2);
             }
+            break;
+    }
+}
+
+num1 = null;
+num2 = null;
+op = null;
+var i = 0;
+var num_array = [''];
+
+function math(num1, op, num2) {
+    for (i = 0; i < num_array.length; i++) {
+        if (!isNaN(num_array[i])) {
+            //is a number
+            if (num1 === null) {
+                num1 = num_array[i];
+                console.log("num1 in array");
+            } else {
+                num2 = num_array[i];
+                console.log("num2 in array");
+                opSwitch()
+            }
+        } else {
+            // is an op
+            op = num_array[i];
         }
     }
 }
 
 
-
-
+//.apply()
+//    .call()
 

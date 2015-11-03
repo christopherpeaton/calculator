4d58a13d4ec2236f349746761a3bac476828852c
Christopher Eaton <christopherpeaton@gmail.com>
2015-11-02 18:17:53 -0800

    adds two numbers and an operator to the array but thats all

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ var i = 0;
 var num_array = [''];
 
 $(document).ready(function () {
-    $(".calculator_container").on('click', 'button', function () {
-        console.log($(this).text());
+    $(".number_pad").on('click', 'button', function () {
+        console.log('number clicked');
+        //console.log($(this).text());
         var button = $(this).text();
         input_digit(button);
     });
     $(".side_column_operators").on('click', 'button', function () {
-        console.log("op:",$(this).text());
+        console.log('operator clicked');
+        //console.log("op:", $(this).text());
         var button = $(this).text();
         input_operator(button);
     });
@@ -31,49 +33,56 @@ function input_operator(op) {
     i++
     num_array[i] = op;
     i++;
+    num_array[i] = '';
     //do operator stuff here
-}
-function calculate() {
-    num1 = null;
-    num2 = null;
-    op = null;
-    var i = 0;
-    var num_array = [''];
-    for (i = 0; i < num_array.length; i++) {
-        if (!isNaN(num_array[i])) {
-            //is a number
-            if (num1 === null) {
-                num1 = num_array[i];
-                console.log("num1 = i")
+    //function operatorSwitch(num1, op, num2) {
+    //    switch (op) {
+    //        case '+':
+    //            total = Number(num1) + Number(num2);
+    //            break;
+    //        case '-':
+    //            total = Number(num1) - Number(num2);
+    //            break;
+    //        case '*':
+    //            total = Number(num1) * Number(num2);
+    //            break;
+    //        case '/':
+    //            if (num2 === 0) {
+    //                alert("need more input");
+    //                return;
+    //            } else {
+    //                total = Number(num1) / Number(num2);
+    //            }
+    //            break;
+    //    }
+    //}
+
+    $(".equals").on("click", 'button', function () {
+        //operatorSwitch();
+        calculate(num1, op, num2);
+        console.log("total")
+    });
+    function calculate(num1, op, num2) {
+        num1 = null;
+        num2 = null;
+        op = null;
+        var i = 0;
+        var num_array = [''];
+        for (i = 0; i < num_array.length; i++) {
+            if (!isNaN(num_array[i])) {
+                //is a number
+                if (num1 === null) {
+                    num1 = num_array[i];
+                    console.log("num1 = i")
+                } else {
+                    num2 = num_array[i];
+                }
             } else {
-                num2 = num_array[i];
+                // is an op
+                op = num_array[i];
             }
-        } else {
-            // is an op
-            op = num_array[i];
-            switch (op) {
-                case '+':
-                    total = Number(num1) + Number(num2);
-                    break;
-                case '-':
-                    total = Number(num1) - Number(num2);
-                    break;
-                case '*':
-                    total = Number(num1) * Number(num2);
-                    break;
-                case '/':
-                    if (num2 === 0) {
-                        alert("need more input");
-                        return;
-                    } else {
-                        total = Number(num1) / Number(num2);
-                    }
-                    break;
-            }
-            alert(total);
         }
     }
-    ;
 }
 
 

a4263e51bdcc88081d289ade422a39131f4e779a
Christopher Eaton <christopherpeaton@gmail.com>
2015-12-03 15:36:33 -0800

    decimal works, clear works

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 /**
- * Created by christophereaton on 10/29/15.
+ * Created by christopher eaton on 10/29/15.
  */
 
 num1 = null;
@@ -7,6 +7,7 @@ num2 = null;
 op = null;
 var i = 0;
 var num_array = [''];
+var can_add_decimal = true;
 
 $(document).ready(function () {
     $(".number_pad").on('click', 'button:not(#equals)', function () {
@@ -20,21 +21,33 @@ $(document).ready(function () {
         //console.log("op:", $(this).text());
         var button = $(this).text();
         input_operator(button);
+
     });
     $("#equals").on("click", function () {
         math(num1, op, num2);
         console.log("total");
     });
-    $("#allClear").on('click', function() {
+    $("#clear").on("click", function () {
+        console.log("clear clicked");
+        clear();
+    });
+    $("#allClear").on('click', function () {
         console.log("allClear clicked");
         allClear();
     })
-
 });
 
-
 function input_digit(numString) {
-    num_array[i] += numString;
+    switch (numString) {
+        case "." :
+            if (can_add_decimal === true) {
+                num_array[i] += numString;
+                can_add_decimal = false;
+            }
+            break;
+        default:
+        num_array[i] = numString;
+    }
     console.log(num_array[i]);
     $("#display_area").text(num_array[i]);
 }
@@ -44,7 +57,26 @@ function input_operator(op) {
     num_array[i] = op;
     i++;
     num_array[i] = '';
+    can_add_decimal = true;
 }
+
+//function checking_for_decimals() {
+//    if
+//    if (can_add_decimal != true)
+//    return false;
+//}
+function multi_decimal() {
+    for (var i = 0; i < num_array.length; i++) {
+        for (var j = 0; j< num_array.length; j++) {
+            if (num_array[i] == num_array[j]) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
+
 //do operator stuff here
 function opSwitch(num1, op, num2) {
     switch (op) {
@@ -97,24 +129,29 @@ function math() {
     }
     updateDisplay(result);
 }
+
 function updateDisplay(display_val) {
     $("#display_area").text(display_val);
 }
-function allClear() {
-    num_array=[''];
-    num1=null;
-    num2=null;
-    op=null;
-    i=0;
-    updateDisplay(num_array)
-}
-function clear() {}
-    num_array.pop()
 
-//clear/ac button
+function allClear() {
+    num_array = [''];
+    num1 = null;
+    num2 = null;
+    op = null;
+    i = 0;
+    updateDisplay(num_array);
+}
+
+function clear() {
+    num_array.pop();
+    updateDisplay(num_array);
+}
+
 //multiple decimals
 //operation repeat
 //successive multi operation
 //partial operand
 //missing operation
 //missing operand
+//keypress
